069cad76b6eff62e3a56b613dd0a5f9d6cfd2c60
Loleus <loleus@gmail.com>
2025-11-16 21:55:43 +0100

    sw_update

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,10 @@
-const CACHE_NAME = 'fullspeed2skid-v2.4.8';
+const CACHE_NAME = 'fullspeed2skid-v2.4.9';
 const ASSETS = [
   // Root and main files
   '/fullspeed2skid/',
   '/fullspeed2skid/index.html',
   '/fullspeed2skid/manifest.json',
   '/fullspeed2skid/favicon.ico',
-
   // assets/audio
   '/fullspeed2skid/assets/audio/game_ambience.mp3',
   '/fullspeed2skid/assets/audio/game_applause.mp3',
@@ -19,13 +18,11 @@ const ASSETS = [
   '/fullspeed2skid/assets/audio/game_crash.mp3',
   '/fullspeed2skid/assets/audio/menu_button.wav',
   '/fullspeed2skid/assets/audio/menu_music.mp3',
-
   // assets/fonts
   '/fullspeed2skid/assets/fonts/Stormfaze.otf',
   '/fullspeed2skid/assets/fonts/skid.ttf',
   '/fullspeed2skid/assets/fonts/punk_kid.ttf',
   '/fullspeed2skid/assets/fonts/Harting.ttf',
-
   // assets/images
   '/fullspeed2skid/assets/images/asphalt.jpg',
   '/fullspeed2skid/assets/images/asphalt1.jpg',
@@ -48,7 +45,6 @@ const ASSETS = [
   '/fullspeed2skid/assets/images/stone2.jpg',
   '/fullspeed2skid/assets/images/stone3.jpg',
   '/fullspeed2skid/assets/images/water.jpg',
-
   // assets/levels
   '/fullspeed2skid/assets/levels/hiscores.json',
   '/fullspeed2skid/assets/levels/scene_1.svg',
@@ -56,23 +52,18 @@ const ASSETS = [
   '/fullspeed2skid/assets/levels/scene_3.svg',
   '/fullspeed2skid/assets/levels/scene_4.svg',
   '/fullspeed2skid/assets/levels/tracks.json',
-
   // assets/style
   '/fullspeed2skid/assets/styles/style.css',
-
   // src/app
   '/fullspeed2skid/src/app/main.js',
   '/fullspeed2skid/src/app/startup.js',
-
   // src/cameras
   '/fullspeed2skid/src/cameras/ClassicCamera.js',
   '/fullspeed2skid/src/cameras/FPVCamera.js',
   '/fullspeed2skid/src/cameras/CameraManager.js',
-
   // src/core
   '/fullspeed2skid/src/core/constants.js',
   '/fullspeed2skid/src/core/phaser.js',
-
   // src/domain/vehicles
   '/fullspeed2skid/src/domain/vehicles/Car.js',
   '/fullspeed2skid/src/domain/vehicles/CarConfig.js',
@@ -86,33 +77,27 @@ const ASSETS = [
   '/fullspeed2skid/src/domain/world/TextureManager.js',
   '/fullspeed2skid/src/domain/world/World.js',
   '/fullspeed2skid/src/domain/world/WorldLoader.js',
-
   // src/input
   '/fullspeed2skid/src/input/controlsManager.js',
   '/fullspeed2skid/src/input/gyro-handler.js',
   '/fullspeed2skid/src/input/keyboardManager.js',
-
   // src/scenes
   '/fullspeed2skid/src/scenes/GameScene.js',
   '/fullspeed2skid/src/scenes/HudScene.js',
   '/fullspeed2skid/src/scenes/LoadingScene.js',
   '/fullspeed2skid/src/scenes/MenuScene.js',
-
   // src/services
   '/fullspeed2skid/src/services/HiscoreService.js',
   '/fullspeed2skid/src/services/VehicleFactory.js',
-
   // src/systems
   '/fullspeed2skid/src/systems/CountdownManager.js',
   '/fullspeed2skid/src/systems/hiscoreManager.js',
   '/fullspeed2skid/src/systems/hudManager.js',
   '/fullspeed2skid/src/systems/LapsTimer.js',
   '/fullspeed2skid/src/systems/SkidMarksSystem.js',
-
   // src/systems/rendering
   '/fullspeed2skid/src/systems/rendering/skidMarks.js',
   '/fullspeed2skid/src/systems/rendering/SkidMarksManager.js',
-
   // src/ui
   '/fullspeed2skid/src/ui/hiscoresOverlay.js',
   '/fullspeed2skid/src/ui/menu_background.js',
@@ -136,32 +121,9 @@ self.addEventListener('install', event => {
       })
   );
   self.skipWaiting();
-  // self.clients.claim();
 });
 
 // Aktywacja service workera
-// self.addEventListener('activate', event => {
-//   console.log('[SW] Activating service worker...');
-//   event.waitUntil(
-//     caches.keys().then(cacheNames => {
-//       return Promise.all(
-//         cacheNames.map(cacheName => {
-//           if (cacheName !== CACHE_NAME) {
-//             console.log('[SW] Deleting old cache:', cacheName);
-//             return caches.delete(cacheName);
-//           }
-//         })
-//       );
-//     })
-//   );
-//   self.clients.claim();
-//   self.clients.matchAll().then(clients => {
-//     clients.forEach(client => {
-//       client.postMessage({ type: 'NEW_VERSION_AVAILABLE' });
-//     });
-//   });
-// });
-
 self.addEventListener('activate', event => {
   console.log('[SW] Activating service worker...');
   event.waitUntil(

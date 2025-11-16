25bd17162bb5ce38e57647807db8e4157ffcf379
Loleus <loleus@gmail.com>
2025-11-16 18:07:24 +0100

    dsa

--- src/app/main.js
+++ src/app/main.js
@@ -1,8 +1,8 @@
 // Import scen gry (ekran ładowania, menu, właściwa rozgrywka, HUD)
-import { MenuScene } from '../scenes/MenuScene.js?v=2.4.2';
-import { LoadingScene } from '../scenes/LoadingScene.js?v=2.4.2';
-import { GameScene } from '../scenes/GameScene.js?v=2.4.2';
-import { HudScene } from '../scenes/HudScene.js?v=2.4.2';
+import { MenuScene } from '../scenes/MenuScene.js?v=2.4.3';
+import { LoadingScene } from '../scenes/LoadingScene.js?v=2.4.3';
+import { GameScene } from '../scenes/GameScene.js?v=2.4.3';
+import { HudScene } from '../scenes/HudScene.js?v=2.4.3';
 
 // Import stałych globalnych
 import { TILE_SIZE, WORLD_HEIGHT, GAME_CONFIG } from '../core/constants.js';
